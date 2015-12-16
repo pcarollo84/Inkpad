ca750c1b3463bc86e91776b99362887172dc8349
Paolo Carollo <paolo.carollo@365agile.com>
2015-12-16 17:03:02 +0000

    now it works

---
+++ InkpadFramework/InkpadFramework.h
@@ -0,0 +1,19 @@
+//
+//  InkpadFramework.h
+//  InkpadFramework
+//
+//  Created by Paolo Carollo on 16/12/2015.
+//  Copyright © 2015 Taptrix, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InkpadFramework.
+FOUNDATION_EXPORT double InkpadFrameworkVersionNumber;
+
+//! Project version string for InkpadFramework.
+FOUNDATION_EXPORT const unsigned char InkpadFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InkpadFramework/PublicHeader.h>
+
+
